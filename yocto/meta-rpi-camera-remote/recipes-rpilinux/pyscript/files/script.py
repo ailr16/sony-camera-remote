a6cd1ae11e7be4bc13112803b016a03a1d0dd49a
Angel Lozano <angel_ivan_5@outlook.com>
2024-05-29 19:05:09 -0600

    Update before deleting docker container

--- yocto/meta-rpi-camera-remote/recipes-rpilinux/pyscript/files/script.py
+++ yocto/meta-rpi-camera-remote/recipes-rpilinux/pyscript/files/script.py
@@ -1,13 +1,33 @@
 import RPi.GPIO as GPIO
 import time
+import tkinter
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)
 
-while True:
-    GPIO.output(8, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(8, GPIO.LOW)
-    time.sleep(1)
+#while True:
+#    GPIO.output(8, GPIO.HIGH)
+#    time.sleep(1)
+#    GPIO.output(8, GPIO.LOW)
+#    time.sleep(1)
 
+def led_on():
+    GPIO.output( 8, GPIO.HIGH )
+
+def led_off():
+    GPIO.output( 8, GPIO.LOW )
+
+window = tkinter.Tk()
+window.title("Basic HMI")
+
+label = tkinter.Label(window, text="Basic HMI using Raspberry Pi 4 and Yocto image")
+label.pack()
+
+button_on = tkinter.Button(window, text="LED On", command = lambda: led_on())
+button_off = tkinter.Button(window, text="LED Off", command = lambda: led_off())
+
+button_on.pack()
+button_off.pack()
+
+window.mainloop()
